67f392fcd1e9e57b6700d9d4e091efea8524e508
Jehan <jehan.tremback@gmail.com>
2014-02-13 02:03:42 -0800

    update example config

--- config/config.example.js
+++ config/config.example.js
@@ -2,34 +2,25 @@
 
 module.exports = function () {
   if (process.env.NODE_ENV === 'production') {
-    return {
-      db: 'mongodb://localhost/noobjs_dev',
-      facebook: {
-        clientID: 'APP_ID',
-        clientSecret: 'APP_SECRET',
-        callbackURL: 'http://localhost:3000/auth/facebook/callback'
-      }
-    };
+
   }
 
   if (process.env.NODE_ENV === 'test') {
-    return {
-      db: 'mongodb://localhost/noobjs_dev',
-      facebook: {
-        clientID: 'APP_ID',
-        clientSecret: 'APP_SECRET',
-        callbackURL: 'http://localhost:3000/auth/facebook/callback'
-      }
-    };
+
   }
 
   else {
     return {
       db: 'mongodb://localhost/noobjs_dev',
+      url: 'https://localhost:3700',
+      sessionSecret: 'This is my funky secret oh my god it has ninja turtles',
+      cryptos: {
+        id: 'ID',
+        secret: 'SECRET'
+      },
       facebook: {
         clientID: 'APP_ID',
-        clientSecret: 'APP_SECRET',
-        callbackURL: 'http://localhost:3000/auth/facebook/callback'
+        clientSecret: 'APP_SECRET'
       }
     };
   }
