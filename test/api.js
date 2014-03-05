059c349e0fd02c6846566909231568b63be786bc
Jehan <jehan.tremback@gmail.com>
2014-03-04 22:14:32 -0800

    fixes to api test

--- test/api.js
+++ test/api.js
@@ -4,8 +4,9 @@ var test = require('tape');
 var mongoose = require('mongoose');
 var request = require('supertest');
 var config = require('../config/config')('test');
-var app = require('../app');
+var app = require('../app.js');
 var passportStub = require('passport-stub-js');
+var utility = require('../test-utility');
 
 
 test('API controller', function (t) {
@@ -13,10 +14,11 @@ test('API controller', function (t) {
   var username = 'Jehoon';
   var provider = 'farcebook';
 
+
   passportStub.install(app);
   var Account = mongoose.model('Account');
 
-  t.test('setup', function () {
+  t.test('setup', function (t) {
 
     account = new Account({
       'providers': [{
@@ -24,35 +26,37 @@ test('API controller', function (t) {
         'provider': provider
       }]
     });
-
-    account.save(function (err, account) {
-      console.log(err, account);
-      t.notOk(err, 'account saves ok');
-      t.end();
+    
+    utility.resetMongo(Account, function () {
+      account.save(function (err, account) {
+        console.log(err, account);
+        t.notOk(err, 'account saves ok');
+        t.end();
+      });
     });
   });
 
-  t.test('smoke test', function () {
+  t.test('smoke test', function (t) {
     request(app)
       .get('/api')
       .expect(200)
-      .end(function (err, res) {
+      .end(function (err) {
         t.notOk(err, '/api returns 200');
         t.end();
       });
   });
 
-  t.test('logged out', function () {
+  t.test('logged out', function (t) {
     request(app)
       .get('/api/user')
       .expect(401)
-      .end(function (err, res) {
+      .end(function (err) {
         t.notOk(err, 'logged out returns 401');
         t.end();
       });
   });
 
-  t.test('logged in', function () {
+  t.test('logged in', function (t) {
     passportStub.login(account);
     request(app)
       .get('/api/user')
