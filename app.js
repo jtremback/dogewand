aba105bfe03ef1239d47bfd1fc72fea695a5933d
Jehan <jehan.tremback@gmail.com>
2014-04-29 23:13:34 -0700

    bootstrap models correctly

--- app.js
+++ app.js
@@ -24,11 +24,9 @@ mongoose.connection.on('error', function (err) {
 
 
 
-// Bootstrap models
-var models_path = __dirname + '/app/models';
-fs.readdirSync(models_path).forEach(function (file) {
-  if (~file.indexOf('.js')) require(models_path + '/' + file);
-});
+require('./app/models/account.js');
+require('./app/models/tip.js');
+
 
 // bootstrap passport config
 require('./config/passport')(passport, config);
