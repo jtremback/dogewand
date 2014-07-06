c301574a0eb61f596b8c4bf350b81485f9b647e9
Jehan <jehan.tremback@gmail.com>
2014-07-05 19:52:43 -0700

    add example config

--- config/config.example.js
+++ config/config.example.js
@@ -1,24 +1,29 @@
 'use strict';
 
+var path = require('path');
+
 module.exports = function (env) {
   var nenv = process.env.NODE_ENV;
 
-  if (nenv === 'production' || env === 'production') {
-
-  }
-
   if (nenv === 'test' || env === 'test') {
     return {
-      db: 'mongodb://localhost/dogewand-test',
+      db: 'pg://jehan@localhost:5432/dogewand-test',
       url: 'https://localhost:3700',
-      sessionSecret: 'This is my funky secret oh my god it has ninja turtles',
+      sessionSecret: 'XXX',
+      bookmarklet_version: 1,
+      root: path.normalize(__dirname + '/..'),
+      port: 3700,
       rpc: {
-        rpcuser: 'dogecoinrpc',
+        rpcuser: 'XXX',
         rpcpassword: 'XXX',
         ip: 'XXX',
-        port: '1337'
+        port: 'XXX'
       },
       facebook: {
+        clientID: 'XXX',
+        clientSecret: 'XXX'
+      },
+      reddit: {
         clientID: 'XXX',
         clientSecret: 'XXX'
       }
@@ -27,16 +32,23 @@ module.exports = function (env) {
 
   else {
     return {
-      db: 'mongodb://localhost/dogewand',
+      db: 'pg://jehan@localhost:5432/dogewand',
       url: 'https://localhost:3700',
-      sessionSecret: 'This is my funky secret oh my god it has ninja turtles',
+      sessionSecret: 'XXX',
+      bookmarklet_version: 1,
+      root: path.normalize(__dirname + '/..'),
+      port: 3700,
       rpc: {
-        rpcuser: 'dogecoinrpc',
+        rpcuser: 'XXX',
         rpcpassword: 'XXX',
         ip: 'XXX',
-        port: '1337'
+        port: 'XXX'
       },
       facebook: {
+        clientID: 'XXX',
+        clientSecret: 'XXX'
+      },
+      reddit: {
         clientID: 'XXX',
         clientSecret: 'XXX'
       }
