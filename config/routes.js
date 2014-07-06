6913926adf84ef12cf6957992578842ed4c929fd
Jehan <jehan.tremback@gmail.com>
2014-07-05 23:48:24 -0700

    add index route

--- config/routes.js
+++ config/routes.js
@@ -16,6 +16,10 @@ module.exports = function (app, passport) {
 
   app.post('/api/v1/user/withdraw', ensureAuthenticated, api.withdraw);
 
+  app.get('/', function (req, res) {
+    return res.send('dogewand');
+  });
+
   app.get('/tips/:tip', pages.tip);
   app.post('/tips/:tip', ensureAuthenticated, pages.resolveTip);
 
