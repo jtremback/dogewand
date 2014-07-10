aaaa06a619795c2ba418fdb10b9246a991b36d51
Jehan <jehan.tremback@gmail.com>
2014-07-09 20:29:10 -0700

    Revert "tests pass for new array in pg"
    This reverts commit b4dd5d7c34338aaa60ae1c37efe9369a54f69fce.

--- app/models/functions.sql
+++ app/models/functions.sql
@@ -6,14 +6,14 @@ DECLARE result RECORD;
 BEGIN
 LOOP
   SELECT * INTO result FROM accounts
-  WHERE $1 = ANY (uniqid)
+  WHERE uniqid = $1
   AND provider = $2;
   IF found THEN
     RETURN result;
   END IF;
   BEGIN
     INSERT INTO accounts (uniqid, provider, display_name)
-    VALUES (ARRAY[$1], $2, $3)
+    VALUES ($1, $2, $3)
     RETURNING * INTO result;
     RETURN result;
   EXCEPTION WHEN unique_violation THEN
@@ -32,7 +32,7 @@ BEGIN
 LOOP
   UPDATE accounts
   SET display_name = $3
-  WHERE $1 = ANY (uniqid)
+  WHERE uniqid = $1
   AND provider = $2
   RETURNING * INTO result;
   IF found THEN
@@ -40,7 +40,7 @@ LOOP
   END IF;
   BEGIN
     INSERT INTO accounts (uniqid, provider, display_name)
-    VALUES (ARRAY[$1], $2, $3)
+    VALUES ($1, $2, $3)
     RETURNING * INTO result;
     RETURN result;
   EXCEPTION WHEN unique_violation THEN
