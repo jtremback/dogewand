66e77adcec5744e32a32cc740d1075d8466edb7a
Jehan <jehan.tremback@gmail.com>
2014-10-07 23:02:04 -0700

    try to expose static

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 var passport = require('passport');
+var path = require('path');
 var config = require('./config/config');
 
 console.log(config)
@@ -9,6 +10,9 @@ console.log(config)
 require('./config/passport')(passport, config);
 
 var app = express();
+
+// Serve static
+app.use('/dist/', express.static(path.join(__dirname, 'static/dist')));
 
 // express settings
 require('./config/express')(app, config, passport);
